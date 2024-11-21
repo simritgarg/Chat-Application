c305fbd581e8d02e99e71b9eedad2a7855e548a3
Simrit garg <simritgarg@gmail.com>
2024-11-22 01:08:02 +0530

    commit changes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,4 +1,12 @@
 import jwt from 'jsonwebtoken';
 const generateTokenAndSetCookie = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {})
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '15d'
+    });
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "Strict",
+     }); 
 };
+export default generateTokenAndSetCookie;
