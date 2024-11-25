4491f2013efb72f4de3a1789edec990452d5a893
Simrit garg <simritgarg@gmail.com>
2024-11-26 02:59:37 +0530

    commit last backend changes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import cookieParser from "cookie-parser";
 // Routes import
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -22,6 +23,7 @@ app.use(express.json()); // to parse the incoming request with the JSON payloads
 app.use(cookieParser()); 
 app.use("/api/auth",authRoutes) //middleware for root routes
 app.use("/api/messages",messageRoutes) 
+app.use("/api/users",userRoutes) 
 
 
 
