c432898ebd798cd2158cf921143fc0a0fa9358eb
Simrit garg <simritgarg@gmail.com>
2024-11-26 02:45:23 +0530

    commit new changes

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,5 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 export const sendMessage = async (req, res) => {
     try{
         const { message } = req.body;
@@ -22,6 +24,13 @@ export const sendMessage = async (req, res) => {
 		if (newMessage) {
 			conversation.messages.push(newMessage._id);
 		}
+		// await conversation.save();
+		// await newMessage.save();
+
+		// this will run in parallel
+		await Promise.all([conversation.save(), newMessage.save()]);
+
+		// SOCKET IO FUNCTIONALITY WILL GO HERE
 
         res.status(201).json(newMessage);
 
@@ -31,3 +40,22 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({error: "Internal Server Error"});
     }
 }
+export const getMessages = async (req, res) => {
+	try {
+		const { id: userToChatId } = req.params;
+		const senderId = req.user._id;
+
+		const conversation = await Conversation.findOne({
+			participants: { $all: [senderId, userToChatId] },
+		}).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+
+		if (!conversation) return res.status(200).json([]);
+
+		const messages = conversation.messages;
+
+		res.status(200).json(conversation.messages);
+	} catch (error) {
+		console.log("Error in getMessages controller: ", error.message);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import User from "../models/user.model.js";
 const protectRoute = async (req, res, next) => {
     try{
         const token = req.cookies.jwt;
